9ab148f5794b0bc901570cc3b627e2d8dfb01e1a
Göran Lilja <goran@lilja.tech>
2022-01-04 00:13:33 +0100

    Add bottom sheet and basic unit tests

--- Sources/BottomSheet/BottomSheet.swift
+++ Sources/BottomSheet/BottomSheet.swift
@@ -1,6 +1,124 @@
-public struct BottomSheet {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct BottomSheet<Content: View>: View {
+    @Binding var isShowingSheet: Bool
+
+    /// Determines whether the line at the top of the sheet should be shown.
+    let showHandleBar: Bool
+
+    /// Determines whether the dismiss button at the bottom of the sheet should be shown.
+    let showDismissButton: Bool
+
+    /// Determines whether the sheet should have rounded corners at the top
+    let showRoundedCorners: Bool
+
+    /// Defines the text of the dismiss button.
+    let dismissText: LocalizedStringKey
+
+    /// Defines the content to be shown in the sheet.
+    let content: Content
+
+    @State private var currentPanningOffset: CGFloat = 0
+    private let minimumPanningThreshold: CGFloat = 20
+
+    public init(
+        isShowingSheet: Binding<Bool>,
+        showHandleBar: Bool = true,
+        showDismissButton: Bool = true,
+        showRoundedCorners: Bool = true,
+        dismissText: LocalizedStringKey = "Dismiss",
+        @ViewBuilder content: () -> Content
+    ) {
+        _isShowingSheet = isShowingSheet
+        self.showHandleBar = showHandleBar
+        self.showDismissButton = showDismissButton
+        self.showRoundedCorners = showRoundedCorners
+        self.dismissText = dismissText
+        self.content = content()
+    }
+
+    public var body: some View {
+        ZStack {
+            Color(UIColor.label)
+                .opacity(0.3)
+                .opacity(isShowingSheet ? 1 : 0)
+                .animation(.easeIn)
+                .onTapGesture {
+                    dismiss()
+                }
+                .ignoresSafeArea()
+
+            GeometryReader { geo in
+                VStack {
+                    Spacer()
+
+                    VStack {
+                        if showHandleBar {
+                            Capsule()
+                                .frame(width: 180, height: 6)
+                                .foregroundColor(Color(UIColor.tertiaryLabel))
+                                .padding(.top, 10)
+                        }
+
+                        content
+
+                        if showDismissButton {
+                            Button(action: dismiss) {
+                                Text(dismissText)
+                                    .bold()
+                                    .foregroundColor(.red)
+                            }
+                        }
+                    }
+                    .padding(.bottom, 40)
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(showRoundedCorners ? 40 : 0, corners: .top)
+                    .animation(.easeInOut)
+                    .offset(y: isShowingSheet ? 0 : geo.size.height)
+                    .offset(y: currentPanningOffset)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                currentPanningOffset = max(0, value.translation.height)
+                            }
+                            .onEnded { value in
+                                if value.translation.height > minimumPanningThreshold {
+                                    dismiss()
+                                }
+                                currentPanningOffset = 0
+                            }
+                    )
+                }
+            }
+            .ignoresSafeArea()
+        }
+    }
+
+    func dismiss() {
+        isShowingSheet.toggle()
     }
 }
+
+extension UIRectCorner {
+    static var top: UIRectCorner { [.topLeft, .topRight] }
+    static var bottom: UIRectCorner { [.bottomLeft, .bottomRight] }
+    static var left: UIRectCorner { [.topLeft, .bottomLeft] }
+    static var right: UIRectCorner { [.topRight, .bottomRight] }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
--- Tests/BottomSheetTests/BottomSheetTests.swift
+++ Tests/BottomSheetTests/BottomSheetTests.swift
@@ -1,11 +1,98 @@
 import XCTest
+import SwiftUI
 @testable import BottomSheet
 
 final class BottomSheetTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BottomSheet().text, "Hello, World!")
+    func testDefaultValues() throws {
+        let sut = BottomSheet(
+            isShowingSheet: .constant(false)
+        ) {
+            Text("Hello World!")
+        }
+
+        XCTAssertFalse(sut.isShowingSheet)
+        XCTAssertTrue(sut.showHandleBar)
+        XCTAssertTrue(sut.showDismissButton)
+        XCTAssertTrue(sut.showRoundedCorners)
+        XCTAssertEqual(sut.dismissText, "Dismiss")
+    }
+
+    func testCustomValues() {
+        let sut = BottomSheet(
+            isShowingSheet: .constant(false),
+            showHandleBar: false,
+            showDismissButton: false,
+            showRoundedCorners: false,
+            dismissText: "Close"
+        ) {
+            Text("Hello World!")
+        }
+
+        XCTAssertFalse(sut.isShowingSheet)
+        XCTAssertFalse(sut.showHandleBar)
+        XCTAssertFalse(sut.showDismissButton)
+        XCTAssertFalse(sut.showRoundedCorners)
+        XCTAssertEqual(sut.dismissText, "Close")
+    }
+
+    func testPresentSheet() {
+        var bindingValue = false
+        let binding = Binding {
+            bindingValue
+        } set: {
+            bindingValue = $0
+        }
+
+        let sut = BottomSheet(
+            isShowingSheet: binding
+        ) {
+            Text("Hello World!")
+        }
+
+        sut.isShowingSheet.toggle()
+
+        XCTAssertTrue(sut.isShowingSheet)
+    }
+
+    func testDismissSheet() {
+        var bindingValue = true
+        let binding = Binding {
+            bindingValue
+        } set: {
+            bindingValue = $0
+        }
+
+        let sut = BottomSheet(
+            isShowingSheet: binding
+        ) {
+            Text("Hello World!")
+        }
+
+        sut.dismiss()
+
+        XCTAssertFalse(sut.isShowingSheet)
+    }
+
+    func testPresentAndDismissSheet() {
+        var bindingValue = false
+        let binding = Binding {
+            bindingValue
+        } set: {
+            bindingValue = $0
+        }
+
+        let sut = BottomSheet(
+            isShowingSheet: binding
+        ) {
+            Text("Hello World!")
+        }
+
+        sut.isShowingSheet.toggle()
+
+        XCTAssertTrue(sut.isShowingSheet)
+
+        sut.dismiss()
+
+        XCTAssertFalse(sut.isShowingSheet)
     }
 }
